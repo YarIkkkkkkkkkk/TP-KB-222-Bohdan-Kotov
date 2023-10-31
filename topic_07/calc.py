d483b1559c0f3d90a366f4d3ac1637f94c81e2f0
Bohdan <bogdankotov358@gmail.com>
2023-10-31 13:50:25 +0200

    modified calculator

--- topic_07/calc.py
+++ topic_07/calc.py
@@ -1,42 +1,46 @@
-from operations import Operations
-
-
-class Calculator(Operations):
-    def __init__(self):
-        self.a = 0
-        self.b = 0
-        self.log_file_name="topic_07\log.txt"
+from operations import *
+class Calculator:
+    def __init__(self, log_file):
+        self._operand1 = 0
+        self._operand2 = 0
+        self.log_file = log_file
     @property
-    def _a(self):
-        return self.a
-    @_a.setter
-    def _a(self, value):
-        self.a = value
+    def operand1(self):
+        return self._operand1
+    @operand1.setter
+    def operand1(self, value):
+        self._operand1 = value
     @property
-    def _b(self):
-        return self._b
-    @_b.setter
+    def operand2(self):
+        return self._operand2
+    @operand2.setter
     def operand2(self, value):
         self._operand2 = value
-
-    def run(self):
-        c=0
-        while c==0:
-            choice = input("Введіть операцію: ")
-
-            if choice == "+":
-                self.plus_log()
-            elif choice == "-":
-                self.minus_log()
-            elif choice == "*":
-                self.mnoj_log()
-            elif choice == "/":
-                self.dil_log()
-            elif choice == 'q':
-                c=5
-            
-            else:
-                print("Неправильний вибір операції")
-
-calculator = Calculator()
-calculator.run()
+        
+        
+    def add(self):
+        result = self.operand1 + self.operand2
+        self._log("Addition", result)
+        return result
+    def subtract(self):
+        result = self.operand1 - self.operand2
+        self._log("Subtraction", result)
+        return result
+    def multiply(self):
+        result = self.operand1 * self.operand2
+        self._log("Multiplication", result)
+        return result
+    def divide(self):
+        if self.operand2 == 0:
+            result = "Error: Division by zero"
+            self._log("Division", result)
+            return result
+        result = self.operand1 / self.operand2
+        self._log("Division", result)
+        return result
+    
+    def _log(self, operation, result):
+        with open(self.log_file, "a") as log:
+            log.write(f"{operation}: operand1={self.operand1}, operand2={self.operand2}, result={result}\n")
+if __name__ == "__main__":
+    main()
--- topic_07/operations.py
+++ topic_07/operations.py
@@ -1,35 +1,24 @@
-from functions import *
-class Operations():
-    def get_numbers(self):
-        self.a = int(input("Введіть перше число: "))
-        self.b = int(input("Введіть друге число: "))
-        return self.a, self.b
-    def log(self, action, data, result):
-        with open(self.log_file_name, "a") as log_file:
-            log_entry = f"Дія: {action}, Дані: {data}, Результат: {result}\n"
-            log_file.write(log_entry)
-
-
-    def plus_log(self):
-        a, b = self.get_numbers()
-        result = plus(a, b)
-        self.log("Додавання", f"{a} + {b}", result)
-        print("Результат додавання:", result)
-
-    def minus_log(self):
-        a, b = self.get_numbers()
-        result = minus(a, b)
-        self.log("Віднямання", f"{a} - {b}", result)
-        print("Результат віднімання:", result)
-
-    def mnoj_log(self):
-        a, b = self.get_numbers()
-        result = mnoj(a, b)
-        self.log("Множення", f"{a} * {b}", result)
-        print("Результат множення:", result)
-
-    def dil_log(self):
-        a, b = self.get_numbers()
-        result = dil(a, b)
-        self.log("Ділення", f"{a} / {b}", result)
-        print("Результат ділення:", result)
+from calc import Calculator
+def main():
+    log_file = "topic_07/log.txt"
+    calc = Calculator(log_file)
+    while True:
+        choice = input("Введіть функцію: ")
+        operand = float(input("Введіть перше число: "))
+        calc.operand1 = operand
+        operand = float(input("Введіть: "))
+        calc.operand2 = operand
+        if choice == '+':
+            result = calc.add()
+            print("Result:", result)
+        elif choice == '-':
+            result = calc.subtract()
+            print("Result:", result)
+        elif choice == '*':
+            result = calc.multiply()
+            print("Result:", result)
+        elif choice == '/':
+            result = calc.divide()
+            print("Result:", result)
+        else:
+            print("Invalid choice. Please try again.")
